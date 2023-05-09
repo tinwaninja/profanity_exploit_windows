cc6897388c0082b1d6670c3cb787dd1a21ccab19
tinwaninja <tinwaninja@gmail.com>
2023-05-09 22:09:29 +0700

    fuck commit

---
+++ Mode.hpp
@@ -0,0 +1,64 @@
+#ifndef HPP_MODE
+#define HPP_MODE
+
+#include <string>
+
+#if defined(__APPLE__) || defined(__MACOSX)
+#include <OpenCL/cl.h>
+#else
+#include <CL/cl.h>
+#endif
+
+#include "types.hpp"
+
+enum HashTarget {
+	ADDRESS,
+	CONTRACT,
+	HASH_TARGET_COUNT
+};
+
+class Mode {
+	private:
+		Mode();
+
+	public:
+		static Mode matching(const std::string strHex);
+		static Mode hashTable(const bool extended, const int skipY);
+		static Mode reverse(const std::string strPublicAddress, const int steps, const bool extended, const bool cache, const int skipX, const int skipY, const bool single);
+		
+		static Mode range(const cl_uchar min, const cl_uchar max);
+		static Mode leading(const char charLeading);
+		static Mode leadingRange(const cl_uchar min, const cl_uchar max);
+		static Mode mirror();
+
+		static Mode benchmark();
+		static Mode zeros();
+		static Mode letters();
+		static Mode numbers();
+		static Mode doubles();
+
+		std::string name;
+
+		std::string kernel;
+
+		HashTarget target;
+		// kernel transform fn name
+		std::string transformKernel() const;
+		// Address, Contract, ...
+		std::string transformName() const;
+
+		cl_uchar data1[20];
+		cl_uchar data2[20];
+		cl_uchar score;
+
+		// Reverse mode
+		point targetAddress;
+		int steps;
+		int skipX;
+		int skipY;
+		bool extended;
+		bool cache;
+		bool single;
+};
+
+#endif /* HPP_MODE */
